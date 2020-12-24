534cfb1c70378da2a9b3422fb1376db5c2066594
Dibakar Roy <dibakar.roy24@gmail.com>
2020-12-24 09:43:32 -0500

    modified backend function of main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,11 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
-    access_key           = ""
+    storage_account_name = "__terraformstorageaccount__"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  access_key           = "__storagekey__"
+  features {}
   }
 }
 
