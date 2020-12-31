a10d46b2bdc4a918d5a2e9b95f441b6f73794fad
dibaroy24 <dibakar.roy24@gmail.com>
2020-12-31 07:50:03 -0500

    Removed disable_password_authentication step from main.tf file as it failed

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,5 +70,5 @@ module "virtual_machine" {
   public_ip_address_id = module.public_ip.public_ip_address_id
   public_subnet_id     = module.network.subnet_id_test
   admin_username       = "vmadmin"
-  disable_password_authentication = true
+  # disable_password_authentication = true
 }
